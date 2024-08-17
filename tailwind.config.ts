9fcee4f9654e21d4e2b6018469ad98b450afeb80
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-17 17:44:53 +0100

    Add colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: {
+          50: "#FFFFFF",
+          100: "#F7F9FC",
+          200: "#F6F7F9",
+        },
       },
     },
   },
