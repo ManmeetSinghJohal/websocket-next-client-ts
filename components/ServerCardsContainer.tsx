6361deb690cba132039982d0aeb35573e817bb76
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-17 17:40:25 +0100

    container for ServerCards

---
+++ components/ServerCardsContainer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ServerCard from "./ServerCard";
+
+type ServerCardsContainerProps = {
+  data: EndpointData[];
+};
+
+const ServerCardsContainer: React.FC<ServerCardsContainerProps> = ({
+  data,
+}) => {
+  return (
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2  border">
+      {data.map((item, index) => (
+        <ServerCard
+          key={index}
+          title={`${(item.data.region || "").toUpperCase()}`}
+          region={item.data.region}
+          status={item.data.status}
+          roles={item.data.roles}
+          strict={item.data.strict}
+          serverIssue={item.data.server_issue}
+          services={item.data.results.services}
+          stats={item.data.results.stats}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ServerCardsContainer;
