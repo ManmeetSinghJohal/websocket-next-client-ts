c2fa49ad4f8258c6d04c6291ccc7101d2041ce78
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-18 14:03:20 +0100

    update WS_URL

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,7 +6,8 @@ import useWebSocket from "react-use-websocket";
 import { RingLoader } from "react-spinners"; 
 
 export default function Home() {
-  const WS_URL = "ws://127.0.0.1:8000";
+  const WS_URL: string = process.env.REACT_APP_WS_URL || "ws://127.0.0.1:8000";
+
   const { lastJsonMessage } = useWebSocket(WS_URL);
 
   // Loading state
