f60eff247a04e0c2d372532d8a045cc0a5549b6a
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-19 19:18:09 +0100

    Add font family, add footer, add styling to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Server Status Monitor",
@@ -16,12 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <main>
           <div className="text-center pt-8 sm:pb-10 sm:pt-20 bg-white-300">
-            <h1 className="text-2xl sm:text-4xl font-bold">Server Status Monitor</h1>
+            <h1 className="text-3xl sm:text-5xl font-light">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                Server Status Monitor
+              </span>
+            </h1>
           </div>
           {children}
+          <Footer />
         </main>
       </body>
     </html>
