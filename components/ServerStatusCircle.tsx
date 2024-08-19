d1220a3a58677b4a34bdaab877b6036ec894322d
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-19 20:00:05 +0100

    Add Link tag to ServerStatusCircle

--- components/ServerStatusCircle.tsx
+++ components/ServerStatusCircle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 const ServerStatusCircle: React.FC<{ status: string; region: string }> = ({
   status,
@@ -10,14 +10,16 @@ const ServerStatusCircle: React.FC<{ status: string; region: string }> = ({
   const borderColor = isStatusOk ? "border-green-500" : "border-red-500";
 
   return (
-    <div className="flex flex-col items-center space-y-2 text-center">
-      <span className={`text-xs font-semibold`}>{region}</span>
-      <div
-        className={`w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center bg-transparent border-2 ${borderColor} rounded-full`}
-      >
-        <span className={`text-xs font-semibold ${textColor}`}>{status}</span>
+    <Link href={`/${region}`}>
+      <div className="flex flex-col items-center space-y-2 text-center transition-transform duration-300 hover:scale-105">
+        <span className={`text-xs font-semibold`}>{region}</span>
+        <div
+          className={`w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center bg-transparent border-2 ${borderColor} rounded-full`}
+        >
+          <span className={`text-xs font-semibold ${textColor}`}>{status}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
