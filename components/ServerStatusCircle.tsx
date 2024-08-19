49bf95f7acfdfdd574a9b3071a9c70ce589f1fd4
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-19 19:24:22 +0100

    Add ServerStatusCircle component

---
+++ components/ServerStatusCircle.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ServerStatusCircle: React.FC<{ status: string; region: string }> = ({
+  status,
+  region,
+}) => {
+  // Determine the color based on the status
+  const isStatusOk = status.toLowerCase() === "ok";
+  const textColor = isStatusOk ? "text-green-500" : "text-red-500";
+  const borderColor = isStatusOk ? "border-green-500" : "border-red-500";
+
+  return (
+    <div className="flex flex-col items-center space-y-2 text-center">
+      <span className={`text-xs font-semibold`}>{region}</span>
+      <div
+        className={`w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center bg-transparent border-2 ${borderColor} rounded-full`}
+      >
+        <span className={`text-xs font-semibold ${textColor}`}>{status}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ServerStatusCircle;
