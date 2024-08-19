3b1c84be9aef190784bcfd5c099cff67803a3f88
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-19 19:23:00 +0100

    Add ServerSatatusCircle component

--- components/ServerCardsContainer.tsx
+++ components/ServerCardsContainer.tsx
@@ -2,25 +2,36 @@ import React from "react";
 import ServerCard from "./ServerCard";
 
 import { ServerCardsContainerProps } from "@/types/types";
+import ServerStatusCircle from "./ServerStatusCircle";
 
 const ServerCardsContainer: React.FC<ServerCardsContainerProps> = ({
   data,
 }) => {
   return (
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-      {data.map((item, index) => (
-        <ServerCard
-          key={index}
-          title={`${(item.data.region || "").toUpperCase()}`}
-          region={item.data.region}
-          status={item.data.status}
-          roles={item.data.roles}
-          strict={item.data.strict}
-          serverIssue={item.data.server_issue}
-          services={item.data.results.services}
-          stats={item.data.results.stats}
-        />
-      ))}
+    <div>
+      <div className="flex justify-center mb-12 gap-2 sm:gap-6">
+        {data.map((item, index) => (
+          <ServerStatusCircle
+            status={item.data.status}
+            region={item.data.region}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+        {data.map((item, index) => (
+          <ServerCard
+            key={index}
+            title={`${(item.data.region || "").toUpperCase()}`}
+            region={item.data.region}
+            status={item.data.status}
+            roles={item.data.roles}
+            strict={item.data.strict}
+            serverIssue={item.data.server_issue}
+            services={item.data.results.services}
+            stats={item.data.results.stats}
+          />
+        ))}
+      </div>
     </div>
   );
 };
