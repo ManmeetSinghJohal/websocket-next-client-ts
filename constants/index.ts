c59a7babccf6596fc42d55dabb4559a59209da9d
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-08-19 19:25:10 +0100

    Add socialLinks array

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+import { SocialLinks } from "@/types/types";
+
+export const socialLinks: SocialLinks[] = [
+  {
+    src: "/assets/socialIcons/linkedindark.svg",
+    alt: "LinkedIn",
+    url: "https://www.linkedin.com/in/manmeetsinghjohal",
+  },
+  {
+    src: "/assets/socialIcons/githubdark.svg",
+    alt: "GitHub",
+    url: "https://github.com/ManmeetSinghJohal",
+  },
+];
